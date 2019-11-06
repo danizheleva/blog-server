6930769ad18c8484a3f6545e746607beabf95732
danielazheleva <xdaniela.z@gmail.com>
2019-11-06 19:30:30 +0000

    Added Maven

--- src/main/java/com/danielazheleva/blog/repository/TripRepository.java
+++ src/main/java/com/danielazheleva/blog/repository/TripRepository.java
@@ -2,7 +2,6 @@ package com.danielazheleva.blog.repository;
 
 import com.danielazheleva.blog.entity.TripEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
